71d1b4987d93a286926080c23405b019f9af71ac
mk-pmb <mk-pmb@users.noreply.github.com>
2022-08-05 04:36:22 +0200

    v0.1.2: Simplify tests

--- test/all.mjs
+++ test/all.mjs
@@ -1,20 +1,14 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import 'p-fatal';
 import 'usnam-pmb';
-
-import test from 'p-tape';
+import pTapeAllInList from 'p-tape-all-in-list';
 
 import makeMap from '../omep.mjs';
 
 import basicTests from './basics.mjs';
 
-const allTests = [
+pTapeAllInList({
+  extraApi: { makeMap },
+}, [
   ...basicTests,
-];
-
-const extraApi = {
-  makeMap,
-};
-
-allTests.forEach(f => test(f.name, t => f(Object.assign(t, extraApi))));
+]);
